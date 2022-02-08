b7408eaa10a06fcae0d47a89902ba823c7f3ab5a
g00d <zhukov000@gmail.com>
2022-02-08 21:18:20 +0400

    Initial commit

---
+++ src/tests/test_ping.py
@@ -0,0 +1,7 @@
+from starlette.testclient import TestClient
+
+
+def test_ping(test_app):
+    response = test_app.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!"}
